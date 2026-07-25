76dc18e62a703a2dd53a697e09d3ad9466784bd9
duckfrog-100 <sunyoungfbss@gmail.com>
2026-07-25 21:51:30 +0900

    feat: add vocabulary domain type

---
+++ types/vocabulary.ts
@@ -0,0 +1,9 @@
+export type SavedWord = {
+  word: string;
+  meaning: string;
+  phonetic?: string;
+  partOfSpeech?: string;
+  exampleSentence?: string;
+  sourceTitle?: string;
+  addedAt: number;
+};
